d945c3d8c7d3810f3da09bfbbe09a0b0865e1d09
Aarnav Jindal <developer.aarnav100@gmail.com>
2020-03-28 19:47:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 onload = function () {
     // create a network
-    var curr_data;
-    var container = document.getElementById('mynetwork');
-    var container2 = document.getElementById('mynetwork2');
-    var genNew = document.getElementById('generate-graph');
-    var solve = document.getElementById('solve');
-    var temptext = document.getElementById('temptext');
+    let curr_data;
+    const container = document.getElementById('mynetwork');
+    const container2 = document.getElementById('mynetwork2');
+    const genNew = document.getElementById('generate-graph');
+    const solve = document.getElementById('solve');
+    const temptext = document.getElementById('temptext');
     // initialise graph options
-    var options = {
+    const options = {
         edges: {
             arrows: {
                 to: true
@@ -26,29 +26,34 @@ onload = function () {
             icon: {
                 face: 'FontAwesome',
                 code: '\uf183',
-                size: 50,  //50,
+                size: 50,
                 color: '#991133',
             }
         }
     };
     // initialize your network!
-    var network = new vis.Network(container);
+    let network = new vis.Network(container);
     network.setOptions(options);
-    var network2 = new vis.Network(container2);
+    let network2 = new vis.Network(container2);
     network2.setOptions(options);
 
     function createData(){
-        sz = Math.floor(Math.random() * 8) + 2;
-        nodes = [];
-        for(i=1;i<=sz;i++){
+        const sz = Math.floor(Math.random() * 8) + 2;
+
+        // Adding people to nodes array
+        let nodes = [];
+        for(let i=1;i<=sz;i++){
             nodes.push({id:i, label:"Person "+i})
         }
         nodes = new vis.DataSet(nodes);
 
-        edges = [];
-        for(i=1;i<=sz;i++){
-            for(j=i+1;j<=sz;j++){
+        // Dynamically creating edges with random amount to be paid from one to another friend
+        const edges = [];
+        for(let i=1;i<=sz;i++){
+            for(let j=i+1;j<=sz;j++){
+                // Modifies the amount of edges added in the graph
                 if(Math.random() > 0.5){
+                    // Controls the direction of cash flow on edge
                     if(Math.random() > 0.5)
                         edges.push({from: i, to: j, label: String(Math.floor(Math.random()*100)+1)});
                     else
@@ -56,16 +61,17 @@ onload = function () {
                 }
             }
         }
-        data = {
+        const data = {
             nodes: nodes,
             edges: edges
         };
-        curr_data = data;
+        return data;
     }
 
     genNew.onclick = function () {
-        createData();
-        network.setData(curr_data);
+        const data = createData();
+        curr_data = data;
+        network.setData(data);
         temptext.style.display = "inline";
         container2.style.display = "none";
     };
@@ -73,33 +79,34 @@ onload = function () {
     solve.onclick = function () {
         temptext.style.display  = "none";
         container2.style.display = "inline";
-        solvedData = solveData();
-        network2.setData(solveData());
+        const solvedData = solveData();
+        network2.setData(solvedData);
     };
 
     function solveData() {
-        data = curr_data;
-        sz = data['nodes'].length;
-        vals = Array(sz).fill(0);
-        for(i=0;i<data['edges'].length;i++) {
-            edge = data['edges'][i];
+        let data = curr_data;
+        const sz = data['nodes'].length;
+        const vals = Array(sz).fill(0);
+        // Calculating net balance of each person
+        for(let i=0;i<data['edges'].length;i++) {
+            const edge = data['edges'][i];
             vals[edge['to'] - 1] += parseInt(edge['label']);
             vals[edge['from'] - 1] -= parseInt(edge['label']);
         }
-        for(i=0;i<sz;i++)
-            console.log(vals[i]);
-        console.log('\n');
 
-        new_edges = [];
-        for(i=0;i<sz;i++){
+        const new_edges = [];
+        for(let i=0;i<sz;i++){
+            // Person has to receive money
             if(vals[i]>0){
-                for(j=0;j<sz && vals[i]>0;j++){
+                for(let j=0;j<sz && vals[i]>0;j++){
+                    // Person has to give money
                     if(vals[j]<0){
+                        // Amount to be received is greater than amount to be paid by J
                         if(vals[j]+vals[i]>=0){
                             new_edges.push({from: j+1, to:i+1, label: String(Math.abs(vals[j]))});
                             vals[i]+=vals[j];
                             vals[j] = 0;
-                        } else{
+                        } else{ // Amount to be received is lesser than amount to be paid by J
                             new_edges.push({from: j+1, to:i+1, label: String(vals[i])});
                             vals[j]+=vals[i];
                             vals[i] = 0;
@@ -117,4 +124,5 @@ onload = function () {
     }
 
     genNew.click();
+
 };
