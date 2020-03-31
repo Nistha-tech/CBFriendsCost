429c600e215583f2316205eb031114b6cba79022
Aarnav Jindal <developer.aarnav100@gmail.com>
2020-03-31 13:28:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+import { BinaryHeap } from './heap.js';
+
 onload = function () {
     // create a network
     let curr_data;
@@ -94,25 +97,35 @@ onload = function () {
             vals[edge['from'] - 1] -= parseInt(edge['label']);
         }
 
-        const new_edges = [];
+        const pos_heap = new BinaryHeap();
+        const neg_heap = new BinaryHeap();
+
         for(let i=0;i<sz;i++){
-            // Person has to receive money
             if(vals[i]>0){
-                for(let j=0;j<sz && vals[i]>0;j++){
-                    // Person has to give money
-                    if(vals[j]<0){
-                        // Amount to be received is greater than amount to be paid by J
-                        if(vals[j]+vals[i]>=0){
-                            new_edges.push({from: j+1, to:i+1, label: String(Math.abs(vals[j]))});
-                            vals[i]+=vals[j];
-                            vals[j] = 0;
-                        } else{ // Amount to be received is lesser than amount to be paid by J
-                            new_edges.push({from: j+1, to:i+1, label: String(vals[i])});
-                            vals[j]+=vals[i];
-                            vals[i] = 0;
-                        }
-                    }
-                }
+                pos_heap.insert([vals[i],i]);
+            } else{
+                neg_heap.insert(([-vals[i],i]));
+                vals[i] *= -1;
+            }
+        }
+
+        const new_edges = [];
+        while(!pos_heap.empty() && !neg_heap.empty()){
+            const mx = pos_heap.extractMax();
+            const mn = neg_heap.extractMax();
+
+            const amt = Math.min(mx[0],mn[0]);
+            const to = mx[1];
+            const from = mn[1];
+
+            new_edges.push({from: from+1, to: to+1, label: String(Math.abs(amt))});
+            vals[to] -= amt;
+            vals[from] -= amt;
+
+            if(mx[0] > mn[0]){
+                pos_heap.insert([vals[to],to]);
+            } else if(mx[0] < mn[0]){
+                neg_heap.insert([vals[from],from]);
             }
         }
 
